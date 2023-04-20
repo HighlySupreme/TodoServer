765d2975dcec8168de977e7ba3e91bde87ea9cc1
HighlySupreme <gal.1999k@gmail.com>
2023-04-20 15:42:06 +0200

    Create task service Created task service with a currently empty function

---
+++ src/main/java/com/todo/services/TaskService.java
@@ -0,0 +1,13 @@
+package com.todo.services;
+
+import com.todo.model.Task;
+
+import java.util.List;
+
+public class TaskService {
+
+    public List<Task> getTasks() {
+        return null;
+    }
+
+}
