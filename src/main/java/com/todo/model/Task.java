cdd6ed316d8127edc904554384c2d92e0039d0e2
HighlySupreme <gal.1999k@gmail.com>
2023-04-20 15:14:35 +0200

    Initialized model Task Initialized model Task with base parameters

---
+++ src/main/java/com/todo/model/Task.java
@@ -0,0 +1,57 @@
+package com.todo.model;
+
+import java.util.Date;
+
+public class Task {
+
+    private int id;
+
+    private String title;
+
+    private Date completionDate;
+
+    private String person;
+
+    private String description;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(Date completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
