857e6f862a1c0cf503f750d2af069268b7733122
HighlySupreme <gal.1999k@gmail.com>
2023-04-21 15:55:16 +0200

    Added Create Task Rest endpoint

--- src/main/java/com/todo/controllers/ITaskController.java
+++ src/main/java/com/todo/controllers/ITaskController.java
@@ -3,9 +3,7 @@ package com.todo.controllers;
 import com.todo.model.Task;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +13,11 @@ public interface ITaskController {
     @Get("/")
     @Produces(MediaType.APPLICATION_JSON)
     HttpResponse<List<Task>> getTasks();
+
+    @Post
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    HttpResponse<Task> createTask(Task task);
 
 
 }
--- src/main/java/com/todo/services/TaskService.java
+++ src/main/java/com/todo/services/TaskService.java
@@ -1,15 +1,34 @@
 package com.todo.services;
 
 import com.todo.model.Task;
+import com.todo.services.dao.TaskDao;
+import io.micronaut.context.annotation.Requires;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Singleton
+@Requires(classes = DataSource.class)
 public class TaskService {
+    private DataSource dataSource;
+
+    private final DatabaseService db = new DatabaseService();
+    @Inject
+    public TaskService() {}
 
     public List<Task> getTasks() {
         return null;
+    }
+
+    public void createTask(Task task) {
+        try {
+            db.useExtension(TaskDao.class, dao -> dao.insertTask(task));
+        } catch (Exception e) {
+            //Log the error to the server with logger or anything similar
+            throw new RuntimeException("ERR_CREATE_TASK");
+        }
     }
 
 }
